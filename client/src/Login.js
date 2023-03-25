04831e0e00ee1242c0cd96ddaf17832cb751fa75
F Godsell <frederickgodsell@gmail.com>
2023-03-25 10:07:30 +0000

    logic for differnt LOGIN_URI variables

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,24 @@
-const LOGIN_URI =
-  process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:8888/login'
-    : 'https://heroku-socials-app-staging.herokuapp.com/login';
+// this login var works
+// const LOGIN_URI =
+//   process.env.NODE_ENV !== 'production'
+//     ? 'http://localhost:8888/login'
+//     : 'https://heroku-socials-app-staging.herokuapp.com/login';
+
+    let LOGIN_URI;
+
+    if (process.env.NODE_ENV === 'development') {
+      LOGIN_URI =  'http://localhost:8888/login'
+    }
+    if (process.env.NODE_ENV === 'staging') { 
+      LOGIN_URI =  'https://heroku-socials-app-staging.herokuapp.com/login';
+    }
+    if (process.env.NODE_ENV === 'production'){
+      LOGIN_URI =  'https://heroku-socials-app-attempt-1.herokuapp.com/login';
+    }
+ 
+   
+
+
 
 const Login = () => {
   return (
