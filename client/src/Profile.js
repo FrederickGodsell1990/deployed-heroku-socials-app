e1c938da37515266b0694cb2a922aac24be6b84a
F Godsell <frederickgodsell@gmail.com>
2023-03-29 22:01:07 +0100

    third time changes comitted

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -26,7 +26,7 @@ const Profile = () => {
     <React.Fragment >
     <button onClick={functionToArtistSearch}>Go to Artist Search Page</button>
     <button onClick={functionToFavouriteArtists}>Go to Favourite Artsist's Page</button>
-    <button onClick={functionToMongoDBFunction}>Go to MongoDB Page - stashed changes not committed - and again</button>
+    <button onClick={functionToMongoDBFunction}>Go to MongoDB Page - stashed changes the third time</button>
     
   </React.Fragment>
   );
